a23d48028c1e1aae7a16c40975e95e27bf174c07
Oscar Santos <oscarsantosmu@gmail.com>
2023-05-13 17:42:25 -0600

    fix: update routes navigationmanager & at a tag

--- src/Scripts/remove-instructions.sh
+++ src/Scripts/remove-instructions.sh
@@ -6,6 +6,7 @@ cd src || exit
 if [ -f "Pages/Index.razor" ]; then
     echo "File exists. Updating file..."
     sed -i 's/await JSRuntime.InvokeAsync<string>("localStorage.getItem", "allChecked");/"true";/g' Pages/Index.razor
+    sed -i 's/NavigationManager.NavigateTo("/background", forceLoad: true);/NavigationManager.NavigateTo("/portfolio-blazor-template/background", forceLoad: true);/g' Pages/Index.razor
 else
     echo "File does not exist"
 fi
@@ -14,6 +15,14 @@ fi
 if [ -f "Shared/NavMenu.razor" ]; then
     echo "File exists. Updating file..."
     sed -i 's/await JSRuntime.InvokeAsync<string>("localStorage.getItem", "allChecked");/"true";/g' Shared/NavMenu.razor
+else
+    echo "File does not exist"
+fi
+
+# Check if file exists
+if [ -f "Pages/ProjectCard.razor" ]; then
+    echo "File exists. Updating file..."
+    sed -i 's/<a href="/project/@data?.Title" class="projectUrl">/<a href="/portfolio-blazor-template/project/@data?.Title" class="projectUrl">/g' Pages/Index.razor
 else
     echo "File does not exist"
 fi
