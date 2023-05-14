8203211199591763edc7c75622d81aa8ec16eafe
Oscar Santos <oscarsantosmu@gmail.com>
2023-05-13 18:14:51 -0600

    ci: split scripts to rmv instruction & update href

---
+++ src/Scripts/update-hrefs.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+cd src || exit
+
+REPO_NAME=$(basename ${{ github.repository }})
+
+# Check if file exists
+if [ -f "Pages/Index.razor" ]; then
+    echo "File exists. Updating file..."
+    sed -i "s|NavigationManager.NavigateTo(\"/background\", forceLoad: true);|NavigationManager.NavigateTo(\"/$REPO_NAME/background\", forceLoad: true);|g" Pages/Index.razor
+else
+    echo "File does not exist"
+fi
+
+# Check if file exists
+if [ -f "Pages/ProjectCard.razor" ]; then
+    echo "File exists. Updating file..."
+    sed -i "s|<a href=\"/project/@data?.Title\" class=\"projectUrl\">|<a href=\"/$REPO_NAME/project/@data?.Title\" class=\"projectUrl\">|g" Pages/ProjectCard.razor
+else
+    echo "File does not exist"
+fi
