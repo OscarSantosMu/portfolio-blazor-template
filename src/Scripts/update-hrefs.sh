5ed8a88ac5cf0315bff8b1982a42e0710270c50e
Oscar Santos <oscarsantosmu@gmail.com>
2023-05-14 10:59:34 -0600

    build: update net7

--- src/Scripts/update-hrefs.sh
+++ src/Scripts/update-hrefs.sh
@@ -12,10 +12,3 @@ else
     echo "File does not exist"
 fi
 
-# Check if file exists
-if [ -f "Pages/ProjectCard.razor" ]; then
-    echo "File exists. Updating file..."
-    sed -i "s|<a href=\"/project/@data?.Title\" class=\"projectUrl\">|<a href=\"/$REPO_NAME/project/@data?.Title\" class=\"projectUrl\">|g" Pages/ProjectCard.razor
-else
-    echo "File does not exist"
-fi
